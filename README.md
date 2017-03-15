872ebab0316a1fbd854035e4487d47382d9e90cc
Devin Chalmers <devin.chalmers@gmail.com>
2017-03-14 21:44:55 -0700

    StatefulDummySwitch -> SerialSwitch

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 
-# "Stateful Dummy Switches" Plugin
+# "Serial Switch" Plugin
 
-With this plugin you can create fake "stateful" switches that do nothing when turned on or off. Such switches can be useful as state variables or triggers in HomeKit. This plugin is a simple derivative of the stateless switch homebridge-dummy plugin.
+[Homebridge](https://github.com/nfarina/homebridge/blob/master/example-plugins/homebridge-samplePlatform/index.js) plugin that pretends to be a lightbulb to allow you to send arbitrary commands out a serial port, e.g., to an Arduino. Set up `config.json` with your messages and device; the messages will be mapped to intervals between 0 and 100. E.g., if you have two messages, the first will be sent by setting the brightness of this "bulb" to anything between 0 and 50, and the second by setting the brightness to 50-100.
+
+This allows you to create more or less arbitrary HomeKit commands by defining scenes that set this brightness value. For example, I have an HDMI switch controlled by an Arduino, and two HomeKit scenes to switch between different inputs: "TV Mode" is brightness to 25%, "Nintendo Mode" is brightness to 75%.
+
+Thanks to [StatefulDummySwitch](https://github.com/mend1/homebridge-statefuldummy) for the simple example plugin.
 
 Example config.json:
 
  ```
+   ...
     "accessories": [
         {
-          "accessory": "StatefulDummySwitch",
-          "name": "My Switch 1"
-        }   
+            "accessory": "SerialSwitch",
+            "name": "Projector",
+            "device": "/dev/cu.usbserial-DC008MZU",
+            "messages": ["1", "2"]
+        }
     ]
+    ...
 
 ```
-
--- index.js
+++ index.js
@@ -2,30 +2,65 @@
 
 var Service, Characteristic;
 
+var SerialPort = require('serialport');
+
 module.exports = function(homebridge) {
  
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   
-  homebridge.registerAccessory("homebridge-statefuldummy", "StatefulDummySwitch", StatefulDummySwitch);
+  homebridge.registerAccessory("homebridge-serialswitch", "SerialSwitch", SerialSwitch);
 }
 
-function StatefulDummySwitch(log, config) {
+function SerialSwitch(log, config) {
   this.log = log;
   this.name = config.name;
   
-  this._service = new Service.Switch(this.name);
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
+  this._serial = new SerialPort(config.device, {
+    baudRate: 9600
+  });
+  this._serial.on('open', this._ready.bind(this));
+  
+  this._messages = config.messages;
+  
+  this._service = new Service.Lightbulb(config.name);
+  this._service.getCharacteristic(Characteristic.Brightness).on('set', this._turn.bind(this));
 }
 
-StatefulDummySwitch.prototype.getServices = function() {
+SerialSwitch.prototype.getServices = function() {
   return [this._service];
 }
 
-StatefulDummySwitch.prototype._setOn = function(on, callback) {
+SerialSwitch.prototype._ready = function() {
+  // have to wait for the port to "warm up"
+  setTimeout(function(){
+    this._ready = true;
+    this.log("Serial port ready.");
+  }.bind(this), 5000);
+}
 
-  this.log("Setting switch to " + on);
+SerialSwitch.prototype._turn = function(amt, callback) {
+  this.log("Switching...");
   
+  var msgCount = this._messages.length;
+  if (msgCount < 1) {
+    return;
+  }
+  
+  var msgNum = 0;
+  var step = 101 / msgCount
+  var msgNum = Math.round(amt / step - 0.5);
+  
+  var message = this._messages[msgNum];
+  this.log("Got brightness " + amt + ", matches message " + msgNum + " for output " + message);
+  
+  this._serial.write(message, function(err) {
+    if (err) {
+      this.log('Serial write error: ', err.message);
+    }
+    this.log('Successfully wrote serial message.');
+  }.bind(this));
+  
+  // TODO: send serial signal
   callback();
 }
